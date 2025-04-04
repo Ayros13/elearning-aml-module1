e1bf105fc23a0b63b4627b4e0c01bf77380a5aae
Ayros13 <sorya.prak@outlook.com>
2025-04-04 14:16:45 +0200

    Update main.jsx

--- aml-elearning-module1/src/main.jsx
+++ aml-elearning-module1/src/main.jsx
@@ -1,9 +1,8 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>✅ AML Module 1 - React + Vite is working!</h1>
+    <h1>✅ AML Module 1 – React + Vite App is working!</h1>
   </React.StrictMode>
 )
